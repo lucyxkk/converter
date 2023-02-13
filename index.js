381f7baffe22312be4d027208372e70a7ba1d180
Łucja Kunikowska <lucja.kun2@gmail.com>
2023-02-13 19:54:20 +0100

    Initial input handling

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const btn = document.querySelector("button");
-let num = document.querySelector("input").value;
-console.log(num);
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
 
+button1.addEventListener("click", convert1);
 
-btn.addEventListener("click", convert);
-
-function convert() {
-    let result = num*2;
-    alert(result);
-
+function convert1() {
+  const num = input1.value;
+  let result = num * 2;
+  input2.value = result;
 }
-    
-    
+
+button2.addEventListener("click", convert2);
+
+function convert2() {
+  const num = input2.value;
+  let result = num * 2;
+  input1.value = result;
+}
