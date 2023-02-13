acd272b9755041eb41693cfab36d85dcb7230d3f
Łucja Kunikowska <lucja.kun2@gmail.com>
2023-02-13 20:33:38 +0100

    Single input handler

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
 
-input1.addEventListener("input", handleInput1);
+input1.addEventListener("input", handleInput);
+input2.addEventListener("input", handleInput);
 
-input2.addEventListener("input", handleInput2);
-
-function handleInput1(e) {
-  let result = convert(e);
-  input2.value = result;
-}
-
-function handleInput2(e) {
-  let result = convert(e);
-  input1.value = result;
-}
-
-function convert(input) {
+function handleInput(e) {
   const num = e.target.value;
-  return input * 2;
+  let result = num * 2;
+  if (e.target === input1) {
+    input2.value = result;
+  } else {
+    input1.value = result;
+  }
 }
