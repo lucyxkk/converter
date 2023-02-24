cdca7ac825b7bd30b11f797205d945f060392b56
Łucja Kunikowska <lucja.kun2@gmail.com>
2023-02-24 15:37:45 +0100

    Remove commented code, center flags

--- index.js
+++ index.js
@@ -12,28 +12,6 @@ let rate;
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
 
-// input1.addEventListener("input", handleInput);
-// input2.addEventListener("input", handleInput);
-
-// function handleInput(e) {
-//   const num = e.target.value;
-
-//   let result;
-
-//   if (num === "") {
-//     result = "";
-//   } else if (isNaN(num)) {
-//     result = "WRONG";
-//   } else {
-//     result = num * rate;
-//   }
-
-//   if (e.target === input1) {
-//     input2.value = result;
-//   } else {
-//     input1.value = result;
-//   }
-
 input1.addEventListener("input", handleInput1);
 input2.addEventListener("input", handleInput2);
 
